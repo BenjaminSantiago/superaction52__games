c46e044ef5783182b54710543fa3dc18fc5f6917
Benjamin Santiago <santiago.benjamin@gmail.com>
2026-01-07 15:10:33 -0500

    JUPITER SCOPE -> ship sprite added, bullet sprite added, ship moves within screen, bullet shoots

--- src/00__JUPITERscope/main.asm
+++ src/00__JUPITERscope/main.asm
@@ -30,6 +30,35 @@
 ; $2105 --> BGMODE
 ; $212C --> TM 
 ; $4200 --> NMITIMEN
+
+;   JOY1H       JOY1L
+;   $4219       $4218
+;15  bit  8   7  bit  0
+; ---- ----   ---- ----
+; BYsS UDLR   AXlr 0000
+; |||| ||||   |||| ||||
+; |||| ||||   |||| ++++- Signature
+; |||| ||||   ||++------ L/R shoulder buttons
+; |||| ||||   ++-------- A/X buttons
+; |||| ++++------------- D-pad
+; ||++------------------ Select (s) and Start (S)
+; ++-------------------- B/Y buttons
+
+;------------------------------------------
+
+
+; VARIABLES 
+;------------------------------------------
+; $0000 - $0220 --> "shadow" of OAM
+.EQU joy1H__c $0221
+.EQU joy1H__p $0222
+.EQU joy1H__h $0223
+
+
+.EQU ship__speed    $0224
+.EQU meteor__speed  $0225
+.EQU bullet__speed  $0226
+.EQU bullet__firing $0227
 ;------------------------------------------
 
 ;where to put this section of code
@@ -42,6 +71,20 @@
 Start:
     ; START UP THE SNES 
     InitSNES            
+
+    ; initialize variables
+    ;--------------------------------
+    lda #$04
+    sta ship__speed
+
+    lda #$02
+    sta meteor__speed
+
+    lda #$08
+    sta bullet__speed
+
+    stz bullet__firing
+    ;--------------------------------
 
     ; Load Palette to VRAM
     ;--------------------------------
@@ -95,17 +138,23 @@ loop_for_bg:
     ; sprite stuff
     ; ------------------------------------------------
 
-    ;load palette for sprites
+    ;load palettes + graphics for sprites
     LoadPalette meteor__01__palette, 128, 16
-
-    ;load tiles to VRAM
     LoadBlockToVRAM meteor__01__image, $4000, $400
+    LoadPalette ship__palette, 144, 16
+    LoadBlockToVRAM ship__palette, $4200, $800    
+    LoadPalette fireball__palette, 160, 16
+    LoadBlockToVRAM fireball__palette, $4600, $400
 
     ;initialize sprites
     ;---------------------------------------------
     ;put RAM "copy" of sprites offscreen
     jsr SpriteInit    
 
+    ; SPRITES THAT COMPOSE THE METEOR
+    ; just do 32x32? 
+    ; re-export the images
+    ; loop through this better regardless
     ;----------------------
     ;initialize sprite properties
     ;center sprite 
@@ -124,7 +173,7 @@ loop_for_bg:
     lda #%00110000
     sta $0003
     ;----------------------------
-
+    
     ;----------------------
     ;initialize sprite properties
     ;center sprite 
@@ -257,20 +306,57 @@ loop_for_bg:
     lda #%00110000
     sta $001F
     ;----------------------------        
+
+    ;SHIP
+    ;----------------------------        
+    lda #(256/2 - 16)
+    sta $0020
+
+    lda #(180)
+    sta $0021
+
+    lda #$21
+    sta $0022
+
+    lda #%00110010
+    sta $0023
+    ;----------------------------        
+
+    ; BULLET(s)
+    ;----------------------------        
+    ; ship x + 8
+    lda $0020
+    clc 
+    adc #$08
+    sta $0024
+
+    ;same as ship starting y
+    lda $0021
+    sta $0025
+
+    lda #$61
+    sta $0026
+
+    lda #%00100100
+    sta $0027
+    ;----------------------------        
+
     ;enable 9th x-bits
     lda #%00000000
     sta $0200
     lda #%00000000
     sta $0201
-    
-    ; Setup Video modes and other stuff, then turn on the screen
-    jsr SetupVideo
-    
-    lda #$80
-    sta $4200 ;Enable NMI
+    lda #%01010110
+    sta $0202
+
+    lda #$81
+    sta $4200 ;Enable NMI + enable joy pad
 
     lda #%01100010      ;8x8 and 16x16 sprites (obj size #0)
     sta $2101
+
+    ; Setup Video modes and other stuff, then turn on the screen
+    jsr SetupVideo
 
     lda #%00010001      ;Enable BG1
     sta $212C
@@ -284,6 +370,8 @@ loop_for_bg:
 forever:
     wai
 
+    ; move meteor(s)
+    ;-------------------
     clc 
     lda $0001
     adc #$02
@@ -323,6 +411,305 @@ forever:
     lda $001D
     adc #$02
     sta $001D
+    ;-------------------
+
+    ; move bullets
+    ;-------------------
+    lda bullet__firing
+    cmp #$01
+    beq +
+
+    jmp _left_check
+
+    ; move the bullet up
++   lda $0025
+    sec
+    sbc bullet__speed
+    sta $0025
+
+    ; check if it is offscreen
+    lda $0025
+    clc 
+    adc #$10
+    cmp #$F0
+    bcs +
+
+    bra _check_y
+
++   jmp _reset_bullet
+
+    ; check collisions between bullet and meteor 
+    ; if bullet y <= meteor bottom sprite y
+_check_y__1:
+    lda $0025
+    sec
+    sbc #$10
+    cmp $0019
+    bcc _check_x__1
+
+    jmp _left_check
+
+    ; if bullex x >= meteor x
+_check_x__1:
+    lda $0024
+    sec 
+    sbc #$10
+    cmp $0018
+    bcs _check_x__2
+
+    jmp _left_check
+
+    ; if bullet x <= meteor x + width
+_check_x__2:
+    lda $0024
+    clc 
+    adc #$20
+    cmp $0018
+    bcc _bullet_plus_meteor
+    ;-------------------
+    jmp _left_check
+
+_bullet_plus_meteor:
+    ;reset meteor parts
+    
+    ; SPRITES THAT COMPOSE THE METEOR
+    ; just do 32x32? 
+    ; re-export the images
+    ; loop through this better regardless
+    ;------------------------------------------------------
+    ;------------------------------------------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2 - 16)
+    sta $0000
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #(-32)
+    sta $0001
+    
+    ;first tile
+    lda #$00
+    sta $0002
+    
+    lda #%00110000
+    sta $0003
+    ;----------------------------
+    
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2)
+    sta $0004
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #(-32)
+    sta $0005
+    
+    ;first tile
+    lda #$02
+    sta $0006
+    
+    lda #%00110000
+    sta $0007
+    ;----------------------------
+
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2 - 16)
+    sta $0008
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #(-16)
+    sta $0009
+    
+    ;first tile
+    lda #$04
+    sta $000A
+    
+    lda #%00110000
+    sta $000B
+    ;----------------------------
+
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2)
+    sta $000C
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #(-16)
+    sta $000D
+    
+    ;first tile
+    lda #$06
+    sta $000E
+    
+    lda #%00110000
+    sta $000F
+    ;----------------------------
+
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2 - 16)
+    sta $0010
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #(00)
+    sta $0011
+    
+    ;first tile
+    lda #$08
+    sta $0012
+    
+    lda #%00110000
+    sta $0013
+    ;----------------------------
+
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2)
+    sta $0014
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #(00)
+    sta $0015
+    
+    ;first tile
+    lda #$0A
+    sta $0016
+    
+    lda #%00110000
+    sta $0017
+    ;----------------------------
+
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2 - 16)
+    sta $0018
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #$10
+    sta $0019
+    
+    ;first tile
+    lda #$0C
+    sta $001A
+    
+    lda #%00110000
+    sta $001B
+    ;----------------------------
+
+    ;----------------------
+    ;initialize sprite properties
+    ;center sprite 
+    ;set x (screen *.5 / width of sprite)
+    lda #(256/2)
+    sta $001C
+    
+    ;set y (screen.height * .5/height of sprite)
+    lda #$10
+    sta $001D
+    
+    ;first tile
+    lda #$0E
+    sta $001E
+    
+    lda #%00110000
+    sta $001F
+    ;------------------------------------------------------
+    ;------------------------------------------------------
+
+    ;reset if bullet is ready to reset
+_reset_bullet:
+    stz bullet__firing
+    lda #%00000100
+    ora $0202
+    sta $0202   ;set off screen
+    lda $0021
+    sta $0025
+    
+    ; check collisions between meteors and ship
+    
+
+;check directional controls
+;---------------------------------------------
+_left_check:
+    lda joy1H__h
+    and #%00000010
+    bne +
+
+    bra _right_check
++
+    lda $0020
+    sec
+    sbc ship__speed
+    sta $0020
+
+    ;if carry is clear set 
+    ;x explicitly to zero
+    bcc +
+
+    bra _right_check
+
++   stz $0020
+
+_right_check:
+    lda joy1H__h
+    and #%00000001
+    bne +
+
+    bra _Y_check
++
+    lda $0020
+    clc
+    adc ship__speed
+    sta $0020
+
+    cmp #$E0    
+    bcs +
+
+    bra _Y_check
+
++   lda #$E0
+    sta $0020  
+
+_Y_check:
+    lda joy1H__h
+    and #%01000000
+    bne +
+    
+    jmp forever
++
+    lda bullet__firing
+    cmp #$00
+    beq +
+
+    jmp forever
++
+    ; set bit for bullet to be on screen
+    lda $0202
+    and #%11111011  ;explicitly set to on screen
+    sta $0202
+
+    ; set bullet x to be ship x + 8
+    lda $0020
+    clc 
+    adc #$08
+    sta $0024
+
+    lda #$01
+    sta bullet__firing 
 
     jmp forever
 
@@ -398,6 +785,8 @@ SetupVideo:
 
     LDA #$01
     STA $420B      ; start DMA
+    ;----------------------------------
+
     plp
     rts
 
@@ -408,10 +797,13 @@ VBlank:
 	pha
 	phx
 	phy
-    
+    ;-----------------
+
     rep #$10    
     sep #$20
     
+    ; "shadow OAM --> actual OAM"
+    ;---------------------------------
     stz $4300
 
     lda #$04
@@ -431,12 +823,35 @@ VBlank:
 
     lda #$01
     sta $420B
+    ;---------------------------------
 
-	PLY 
-	PLX 
-	PLA 
+    ;get joypad status 
+- 
+    lda $4212
+    and #$01
+    bne - 
 
-    RTI
+    ;read controler 1 high
+    ; DO I NEED TO COPY THIS?
+    ldy joy1H__c
+    lda $4219
+    sta joy1H__c
+    tya
+    eor joy1H__c
+    and joy1H__c
+    sta joy1H__p
+    tya 
+    and joy1H__c
+    sta joy1H__h
+
+    lda $4210
+
+    ;-----------------
+    ply
+    plx 
+    pla 
+	
+    rti
 .ENDS
 
 ; DATA
@@ -456,6 +871,20 @@ VBlank:
     meteor__01__image:
         .incbin "_graphics/meteor__01.pic"
 
+    ship__palette: 
+        .incbin "_graphics/ship__v01.clr"
+    
+    ship__image:
+        .incbin "_graphics/ship__v01.pic"
 .ENDS
 
+.BANK 2 SLOT 0
+.ORG 0 
+.SECTION "MORECharacterData"
+    fireball__palette: 
+        .incbin "_graphics/fireball__v01.clr"
+
+    fireball__image:
+        .incbin "_graphics/fireball__v01.pic"
+.ENDS
 ;------------------------------------------
